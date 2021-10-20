289e48b7318651b6f0e0ab96be23bf4b8ba80cc7
anyiquro <anyizarethquro@hotmail.com>
2021-10-20 17:54:32 -0500

    Canva 1

---
+++ dom/canva/dibujo.js
@@ -0,0 +1,15 @@
+var d = document.getElementById("dibujito"); //para traer el id
+var lienzo = d.getContext("2d"); //metodo del canva
+
+dibujarLinea("pink", 10, 300, 200, 10);
+dibujarLinea("blue", 300, 310, 10, 220);
+
+function dibujarLinea(color, xin, yin, xfin, yfin)
+{
+    lienzo.beginPath();
+    lienzo.strokeStyle = color; //esto era un atributo por eso se usa el =
+    lienzo.moveTo(xin, yin); // se va poneindo el lapiz en el rpiemr punto
+    lienzo.lineTo(xfin, yfin);
+    lienzo.stroke(); //para que se dibuje del color de eleccion
+    lienzo.closePath(); //termina de dibujar
+}
