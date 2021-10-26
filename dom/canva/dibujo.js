3dd87ee8c65681b9cb91fd9b9cbaa319b0a5b927
anyiquro <anyizarethquro@hotmail.com>
2021-10-26 18:08:31 -0500

    Poniendo las lineas que se quieren poner

--- dom/canva/dibujo.js
+++ dom/canva/dibujo.js
@@ -1,38 +1,11 @@
+var texto = document.getElementById("texto_lineas");
+var boton = document.getElementById("botoncito");
+boton.addEventListener("click", dibujoPorClick);
+
 var d = document.getElementById("dibujito"); //para traer el id
+var ancho = d.width;
 var lienzo = d.getContext("2d"); //metodo del canva
-var lineas = 30;
-var l= 0;
-var colorcito = "Black"
-var yi, xf, xi, yf,yini, xini,yfinal, xfinal;
 
-
-while(l < lineas)
-{
-    yi= 10 * l;
-    xf = 10 * (l + 1);
-    dibujarLinea("red", 0, yi, xf, 300);
-    xi= 300 - (10 * l);
-    yf = 10 * (l + 1);
-    dibujarLinea("pink", xi, 0, 0, yf);
-    yini= 300 - (10 * l);
-    xfinal = 290 - (10 * l);
-    dibujarLinea("orange", 300, yini, xfinal, 0); 
-    xini= 10 * (l +1);
-    yfinal = 290 - (10 * l);
-    dibujarLinea("yellow", xini, 300, 300, yfinal);
-    
-    
-    console.log("Linea "+l);
-    
-    l = l + 1;
-}
-
-
-
-dibujarLinea(colorcito, 1 ,1 , 1, 299);
-dibujarLinea(colorcito, 1 ,299 , 299, 299);
-dibujarLinea(colorcito, 1 , 1 , 299, 1);
-dibujarLinea(colorcito, 299 ,1 , 299, 299);
 
 function dibujarLinea(color, xin, yin, xfin, yfin)
 {
@@ -42,4 +15,40 @@ function dibujarLinea(color, xin, yin, xfin, yfin)
     lienzo.lineTo(xfin, yfin);
     lienzo.stroke(); //para que se dibuje del color de eleccion
     lienzo.closePath(); //termina de dibujar
+}
+
+function dibujoPorClick()
+{
+    var lineas = parseInt(texto.value);
+    var l= 0;
+    var colorcito = "Black"
+    var yi, xf, xi, yf,yini, xini,yfinal, xfinal;
+    var espacio = ancho/lineas;
+
+
+while(l < lineas)
+{
+    yi= espacio * l;
+    xf = espacio * (l + 1);
+    dibujarLinea("red", 0, yi, xf, 300); 
+    xi= 300 - (espacio * l);
+    yf = espacio * (l + 1);
+    dibujarLinea("pink", xi, 0, 0, yf);
+    xini= espacio * (l +1);
+    yfinal = (300-espacio) - (espacio * l);
+    dibujarLinea("yellow", xini, 300, 300, yfinal);
+    yini= 300 - (espacio * l);
+    xfinal = (300-espacio) - (espacio * l);
+    dibujarLinea("orange", 300, yini, xfinal, 0); 
+ 
+    
+    console.log("Linea "+l);
+    
+    l = l + 1;
+}
+
+dibujarLinea(colorcito, 1 ,1 , 1, 299);
+dibujarLinea(colorcito, 1 ,299 , 299, 299);
+dibujarLinea(colorcito, 1 , 1 , 299, 1);
+dibujarLinea(colorcito, 299 ,1 , 299, 299);
 }
