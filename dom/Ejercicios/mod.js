bf38cdb3b6fb8176622d41dda2c1d9c49294d4d1
anyiquro <anyizarethquro@hotmail.com>
2021-11-12 16:06:31 -0500

    Finished exercise

--- dom/Ejercicios/mod.js
+++ dom/Ejercicios/mod.js
@@ -1,26 +1,43 @@
-var numero=100;
+
+
+let number = prompt("Cuantos numeros quieres evaluar?");
 
 var divisble = false;
 
-for(var i = 1; i <= 100; i++ )
+for(var i = 1; i <= number; i++ )
 {
     divisible=false;
-    if(i % 3 == 0)
+    if(esDivisible(i,3))
     {
         document.write ("Fizz");
         divisible = true;
     }
 
-    if(i % 5 == 0)
+    if(esDivisible(i,5))
     {
         document.write ("Buzz");
         divisible = true;
     }
     
-    if(!divisible) //Si no es divisible.... se podia if(i%3 !=0 && i % 5 ! =0)
+    if(!esDivisible(i,3) && !esDivisible(i,5)) //Si no es divisible.... se podia if(i%3 !=0 && i % 5 ! =0)
     {
     document.write(i);
     }
     document.write("<br />");
+
+}
+
+
+
+function esDivisible(num, divisor)
+{
+    if(num % divisor == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 
 }
