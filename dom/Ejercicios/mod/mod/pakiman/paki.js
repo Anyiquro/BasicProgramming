406dbe75c430ddf36b3fd77905ec3e79ee69c5cc
anyiquro <anyizarethquro@hotmail.com>
2021-11-24 12:19:09 -0500

    Finished

--- dom/Ejercicios/mod/mod/pakiman/paki.js
+++ dom/Ejercicios/mod/mod/pakiman/paki.js
@@ -30,13 +30,13 @@ class Pakiman
     }
 }
 
-var cauchin = new Pakiman("Cauchin", 100, 30);
-var pokacho = new Pakiman("Pokacho",80, 50);
-var tocinauro = new Pakiman("Tocinauro", 120, 40);
 
 var coleccion =[];
-coleccion.push(cauchin);
-coleccion.push(pokacho);
-coleccion.push(tocinauro);
+coleccion.push(new Pakiman("Cauchin", 100, 30));
+coleccion.push(new Pakiman("Pokacho",80, 50));
+coleccion.push(new Pakiman("Tocinauro", 120, 40));
 
-console.log(coleccion);
+for(var pakin in coleccion)
+{
+    pakin.mostrar();
+}
