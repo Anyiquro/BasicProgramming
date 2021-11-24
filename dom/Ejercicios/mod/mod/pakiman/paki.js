7c53d9366ee0e6a5d0fbdb2e91545c206cf80036
anyiquro <anyizarethquro@hotmail.com>
2021-11-24 12:11:03 -0500

    Antes de quitar las variables

--- dom/Ejercicios/mod/mod/pakiman/paki.js
+++ dom/Ejercicios/mod/mod/pakiman/paki.js
@@ -1,9 +1,42 @@
+var imagenes = [];
+imagenes["Cauchin"] = "vaca.png";
+imagenes["Pokacho"] = "pollo.png";
+imagenes["Tocinauro"] = "cerdo.png";
+
 class Pakiman
 {
-    constructor()
+    constructor(n, v, a)
     {
-        tipo = "Tierra"
+        this.imagen = new Image();
+        this.nombre= n;
+        this.vida = v;
+        this.ataque = a;
+
+        this.imagen.src = imagenes[this.nombre];
+
+    }
+    hablar()
+    {
+        alert(this.nombre);
+    }
+    mostrar()
+    {
+        document.body.appendChild(this.imagen);
+        document.write("<p>");
+        document.write("<strong>" + this.nombre +"</strong>" + "<br/>");
+        document.write("Vida: " + this.vida + "<br>");
+        document.write("Ataque: " + this.ataque);
+        document.write("</p>" + "<hr/>");
     }
 }
 
-var cauchin = new Pakiman();
+var cauchin = new Pakiman("Cauchin", 100, 30);
+var pokacho = new Pakiman("Pokacho",80, 50);
+var tocinauro = new Pakiman("Tocinauro", 120, 40);
+
+var coleccion =[];
+coleccion.push(cauchin);
+coleccion.push(pokacho);
+coleccion.push(tocinauro);
+
+console.log(coleccion);
