444caebf0ed31dbc6d61b58393a15b72057e8c90
anyiquro <anyizarethquro@hotmail.com>
2021-11-24 12:20:31 -0500

    Finishedd

--- dom/Ejercicios/mod/mod/pakiman/paki.js
+++ dom/Ejercicios/mod/mod/pakiman/paki.js
@@ -36,7 +36,7 @@ coleccion.push(new Pakiman("Cauchin", 100, 30));
 coleccion.push(new Pakiman("Pokacho",80, 50));
 coleccion.push(new Pakiman("Tocinauro", 120, 40));
 
-for(var pakin in coleccion)
+for(var pakin of coleccion)
 {
     pakin.mostrar();
 }
